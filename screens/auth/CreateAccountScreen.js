ed701c7f322407d5cefa8edda1ae971cde0f77d0
Josh White <joshwhite@Joshs-MacBook-Pro.local>
2020-01-09 20:30:31 -0500

    Dynamic degree specialty fetching

--- screens/auth/CreateAccountScreen.js
+++ screens/auth/CreateAccountScreen.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button, FlatList, Modal, Text, TextInput, View } from 'react-native';
+import axios from 'axios';
 
 import { AuthContext } from '../../AuthContext';
 
-import { degrees } from '../../degrees';
-
 const CreateAccountScreen = ({ navigation }) => {
   const { signUp } = React.useContext(AuthContext);
-  const [modalVisible, setModalVisible] = React.useState(false);
+
+  const [degreeModalVisible, setDegreeModalVisible] = React.useState(false);
+  const [specialtyModalVisible, setSpecialtyModalVisible] = React.useState(
+    false
+  );
+
+  const [degrees, setDegrees] = React.useState([]);
+  const [specialties, setSpecialties] = React.useState([]);
   const [userInfo, setUserInfo] = React.useState({
     firstName: '',
     lastName: '',
@@ -15,7 +21,8 @@ const CreateAccountScreen = ({ navigation }) => {
     degree: {
       Degree_ID: null,
       Degree_Name: ''
-    }
+    },
+    specialty: {}
   });
 
   const handleInput = (key, val) => {
@@ -25,37 +32,86 @@ const CreateAccountScreen = ({ navigation }) => {
     });
   };
 
-  const renderItem = ({ item }) => (
+  // Fetch degrees on load
+  useEffect(() => {
+    console.log('fetching degrees');
+
+    axios
+      .get(`https://www.eeds.com/ajax_functions.aspx?Function_ID=142`)
+      .then(({ data }) => setDegrees(data));
+  }, []);
+
+  // Fetch specialties when degree changes
+  useEffect(() => {
+    console.log('fetching specialties');
+
+    axios
+      .get(
+        `https://www.eeds.com/ajax_functions.aspx?Function_ID=88&Degree_ID=${userInfo.degree.Degree_ID}`
+      )
+      .then(({ data }) => setSpecialties(data));
+  }, [userInfo.degree]);
+
+  const renderItemDegree = ({ item }) => (
     <Text
       style={{ fontSize: 16, marginBottom: 12 }}
       onPress={() => {
         handleInput('degree', item);
-        setModalVisible(false);
+        setDegreeModalVisible(false);
       }}
     >
       {item.Degree_Name}
     </Text>
   );
 
+  const renderItemSpecialty = ({ item }) => (
+    <Text
+      style={{ fontSize: 16, marginBottom: 12 }}
+      onPress={() => {
+        handleInput('specialty', item);
+        setSpecialtyModalVisible(false);
+      }}
+    >
+      {item.Specialty_Name}
+    </Text>
+  );
+
   return (
     <View style={{ flex: 1 }}>
-      <Modal animationType="slide" transparent={false} visible={modalVisible}>
+      {/* Degree Modal */}
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={degreeModalVisible}
+      >
         <View
           style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
         >
           <Text>Hello World!</Text>
           <FlatList
             data={degrees}
-            renderItem={renderItem}
+            renderItem={renderItemDegree}
             keyExtractor={item => item.Degree_ID.toString()}
             style={{ width: '100%' }}
           />
+        </View>
+      </Modal>
 
-          <Button
-            title="Hide Modal"
-            onPress={() => {
-              setModalVisible(!modalVisible);
-            }}
+      {/* Specialty Modal */}
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={specialtyModalVisible}
+      >
+        <View
+          style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+        >
+          <Text>Hello World!</Text>
+          <FlatList
+            data={specialties}
+            renderItem={renderItemSpecialty}
+            keyExtractor={item => item.Specialty_ID.toString()}
+            style={{ width: '100%' }}
           />
         </View>
       </Modal>
@@ -77,7 +133,14 @@ const CreateAccountScreen = ({ navigation }) => {
         onChangeText={val => handleInput('email', val)}
       />
 
-      <Button title="Select Degree" onPress={() => setModalVisible(true)} />
+      <Button
+        title="Select Degree"
+        onPress={() => setDegreeModalVisible(true)}
+      />
+      <Button
+        title="Select Specialty"
+        onPress={() => setSpecialtyModalVisible(true)}
+      />
 
       <Button title="Sign Up" onPress={signUp} />
       <Text>{JSON.stringify(userInfo)}</Text>
