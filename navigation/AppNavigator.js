95fc93678441d1b10fdf82b5f9a54361737bb60c
Josh White <joshwhite@Joshs-iMac.local>
2020-01-20 16:38:08 -0500

    Simplify

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,21 +1,15 @@
-import React, { useContext } from 'react';
-import { Button } from 'react-native';
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import WebViewScreen from '../screens/WebViewScreen';
-import { AuthContext } from '../AuthContext';
 
 const Stack = createStackNavigator();
 
-export const AppNavigator = props => {
-  const { signOut } = useContext(AuthContext);
-
-  return (
-    <Stack.Navigator headerMode="none">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="WebView" component={WebViewScreen} />
-    </Stack.Navigator>
-  );
-};
+export const AppNavigator = props => (
+  <Stack.Navigator headerMode="none">
+    <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen name="WebView" component={WebViewScreen} />
+  </Stack.Navigator>
+);
 
 export default AppNavigator;
