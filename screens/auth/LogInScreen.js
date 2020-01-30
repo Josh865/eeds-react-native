095c178e99761ca358c52db340755fab8f0883e6
Josh White <joshwhite@Joshs-iMac.local>
2020-01-29 22:40:33 -0500

    Use Formik for form

--- screens/auth/LogInScreen.js
+++ screens/auth/LogInScreen.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import axios from 'axios';
+import { Formik } from 'formik';
 import {
   Button,
   Divider,
@@ -9,10 +10,12 @@ import {
   Input,
   Layout,
   Spinner,
+  Text,
   TopNavigation,
   TopNavigationAction
 } from '@ui-kitten/components';
 import { Appearance } from 'react-native-appearance';
+import * as Yup from 'yup';
 
 // SVG images
 import Doctors from '../../assets/doctors.svg';
@@ -24,71 +27,72 @@ const LogInScreen = ({ route, navigation }) => {
   // Get the select log in method passed from the select method page
   const { logInMethod: logInMethodParam, customField } = route.params;
 
-  // Set up reactive component state
-  const [value, setValue] = useState('');
-  const [busy, setBusy] = useState(false);
-
   const availableLogInMethods = {
     pin: {
       label: 'PIN',
       image: null,
       instructions: null,
       keyboardType: 'numeric',
-      url: `https://www.eeds.com/ajax_functions.aspx?Function_ID=5&PIN=${value.trim()}`
+      url: `https://www.eeds.com/ajax_functions.aspx?Function_ID=5&PIN=`,
+      validationSchema: Yup.object({
+        value: Yup.string()
+          .trim()
+          .required('Required')
+          .length(8, 'Your PIN must be exactly eight numbers')
+      })
     },
     email: {
       label: 'Email',
       image: null,
       instructions: null,
       keyboardType: 'email-address',
-      url: `https://www.eeds.com/ajax_functions.aspx?Function_ID=50&Email_Address=${value.trim()}`
+      url: `https://www.eeds.com/ajax_functions.aspx?Function_ID=50&Email_Address=`,
+      validationSchema: Yup.object({
+        value: Yup.string()
+          .trim()
+          .required('Required')
+          .email('Please enter a valid email address')
+      })
     },
     phone: {
       label: 'Phone',
       image: null,
       instructions: null,
       keyboardType: 'phone-pad',
-      url: `https://www.eeds.com/ajax_functions.aspx?Function_ID=50&Phone_Number=${value.trim()}`
+      url: `https://www.eeds.com/ajax_functions.aspx?Function_ID=50&Phone_Number=`,
+      validationSchema: Yup.object({
+        value: Yup.string()
+          .trim()
+          .required()
+      })
     },
     custom: {
       label: customField?.Custom_Field_Name,
       image: `https://www.eeds.com/${customField?.Login_Logo}`,
       instructions: customField?.Login_Instructions,
       keyboardType: 'default',
-      url: `https://www.eeds.com/ajax_functions.aspx?Function_ID=5&PIN=${value.trim()}&Custom_Field_ID=${
-        customField?.Custom_Field_ID
-      }
-      }`
+      url: `https://www.eeds.com/ajax_functions.aspx?Function_ID=5&Custom_Field_ID=${customField?.Custom_Field_ID}&PIN=`,
+      validationSchema: Yup.object({
+        value: Yup.string()
+          .trim()
+          .required('Required')
+      })
     }
   };
 
   const selectedLogInMethod = availableLogInMethods[logInMethodParam];
 
   // Make sure there's a PIN associated with the crendentials the user entered
-  const fetchPinStatus = async () => {
-    const trimmedValue = value.trim();
-
-    // TODO: Move these to dedicated validation function or switch to Formik
-    if (trimmedValue.length === 0) {
-      Alert.alert(`Please enter your ${selectedLogInMethod.label}.`);
-      return;
-    }
-    if (selectedLogInMethod.label === 'PIN' && trimmedValue.length !== 8) {
-      Alert.alert('Your PIN must be exactly eight numbers.');
-      return;
-    }
-
-    setBusy(true);
-
+  const fetchPinStatus = async ({ value }, setSubmitting) => {
     // Hit server to ensure there's an account associated with the provided credentials
-    const { data } = await axios.get(selectedLogInMethod.url);
+    const { data } = await axios.get(selectedLogInMethod.url + value);
 
     // Stop if we couldn't find a PIN matching the provided credentials
     if (data.PIN_Status === false) {
       Alert.alert(
         `We couldn't find an account associated with the ${selectedLogInMethod.label} you entered.`
       );
-      setBusy(false);
+      setSubmitting(false); // Let Formik know we're no longer submitting
       return;
     }
 
@@ -100,9 +104,7 @@ const LogInScreen = ({ route, navigation }) => {
       logInMethodLabel: selectedLogInMethod.label
     });
 
-    // TODO: This is kind of janky since it causes the button to reappear before the
-    // screen transition finishes
-    setBusy(false);
+    setSubmitting(false); // Let Formik know we're no longer submitting
   };
 
   const HeaderImage = imageProps => {
@@ -160,37 +162,73 @@ const LogInScreen = ({ route, navigation }) => {
           <Layout style={{ marginVertical: 20 }}>
             <HeaderImage />
           </Layout>
-          <Input
-            placeholder={`Enter Your ${selectedLogInMethod.label}`}
-            value={value}
-            size="large"
-            caption={selectedLogInMethod.instructions}
-            keyboardType={selectedLogInMethod.keyboardType}
-            keyboardAppearance={
-              deviceThemeSetting === 'dark' ? 'dark' : 'default'
+          <Formik
+            initialValues={{ value: '' }}
+            validationSchema={selectedLogInMethod.validationSchema}
+            onSubmit={(values, { setSubmitting }) =>
+              fetchPinStatus(values, setSubmitting)
             }
-            returnKeyType="go"
-            autoFocus={true}
-            autoCorrect={false}
-            autoCapitalize="none"
-            onChangeText={setValue}
-            onSubmitEditing={fetchPinStatus}
-          />
-          <Layout
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-              marginTop: 6
-            }}
           >
-            {busy ? (
-              <Spinner size="large" />
-            ) : (
-              <Button size="large" onPress={fetchPinStatus}>
-                Log In
-              </Button>
+            {({
+              values,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              errors,
+              touched,
+              isSubmitting
+            }) => (
+              <>
+                <Input
+                  name="value"
+                  placeholder={`Enter Your ${selectedLogInMethod.label}`}
+                  value={values.value}
+                  size="large"
+                  caption={errors.value && touched.value ? errors.value : ''}
+                  status={errors.value && touched.value ? 'danger' : 'basic'}
+                  keyboardType={selectedLogInMethod.keyboardType}
+                  keyboardAppearance={
+                    deviceThemeSetting === 'dark' ? 'dark' : 'default'
+                  }
+                  maxLength={logInMethodParam === 'pin' ? 8 : 50}
+                  returnKeyType="go"
+                  autoFocus={true}
+                  autoCorrect={false}
+                  autoCapitalize="none"
+                  onChangeText={handleChange('value')}
+                  onBlur={handleBlur('value')}
+                />
+                {selectedLogInMethod.instructions ? (
+                  <Text
+                    category="c1"
+                    appearance="hint"
+                    style={{
+                      alignSelf: 'flex-start',
+                      marginTop: errors.value && touched.value ? 6 : 0,
+                      marginBottom: 12
+                    }}
+                  >
+                    {selectedLogInMethod.instructions}
+                  </Text>
+                ) : null}
+                <Layout
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    marginTop: 6
+                  }}
+                >
+                  {isSubmitting ? (
+                    <Spinner size="large" />
+                  ) : (
+                    <Button size="large" onPress={handleSubmit}>
+                      Log In
+                    </Button>
+                  )}
+                </Layout>
+              </>
             )}
-          </Layout>
+          </Formik>
         </Layout>
       </SafeAreaView>
     </Layout>
