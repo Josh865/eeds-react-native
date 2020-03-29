957f2f894502280d6d797b96ce74f26e3a0b5cb6
Josh White <joshwhite@Joshs-iMac.local>
2020-03-29 16:02:47 -0400

    Remove unnecessary theme context

--- components/AppProviders.js
+++ components/AppProviders.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import {
-  mapping,
-  light as lightTheme,
-  dark as darkTheme,
-} from '@eva-design/eva';
+import { mapping, light, dark } from '@eva-design/eva';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { Appearance } from 'react-native-appearance';
 
 // Context
 import { AuthContextProvider } from '../AuthContext';
-import { ThemeContext } from '../ThemeContext';
 
 // Determine which UI theme object to use for our app based on the user's device theme.
-const theme = Appearance.getColorScheme() === 'dark' ? darkTheme : lightTheme;
+const theme = Appearance.getColorScheme() === 'dark' ? dark : light;
 
 const AppProviders = ({ authContext, children }) => (
   <AuthContextProvider value={authContext}>
     <IconRegistry icons={EvaIconsPack} />
-    <ThemeContext.Provider value={{ theme }}>
-      <ApplicationProvider mapping={mapping} theme={theme}>
-        <SafeAreaProvider>{children}</SafeAreaProvider>
-      </ApplicationProvider>
-    </ThemeContext.Provider>
+    <ApplicationProvider mapping={mapping} theme={theme}>
+      <SafeAreaProvider>{children}</SafeAreaProvider>
+    </ApplicationProvider>
   </AuthContextProvider>
 );
 
