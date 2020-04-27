cc4fb5d0f339d68c07f0d344886405f886ce6cac
Josh White <joshwhite@Joshs-iMac.local>
2020-04-26 20:29:27 -0400

    Fix keyboard handling

--- screens/SignInToEventScreen.js
+++ screens/SignInToEventScreen.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Keyboard, ScrollView, TouchableWithoutFeedback } from 'react-native';
+import { Keyboard, ScrollView } from 'react-native';
 import {
   Button,
   Card,
@@ -14,11 +14,15 @@ import {
   TopNavigationAction,
 } from '@ui-kitten/components';
 
+import { useUser } from '../context/user-context';
+
 const SignInToEventScreen = ({ navigation }) => {
+  const { pin } = useUser();
+
   const [signInCode, setSignInCode] = useState('');
-  const pin = '99001200';
 
   const doSignIn = () => {
+    console.log(`signing in with pin ${pin} and code ${signInCode}`);
     navigation.navigate('WebView', {
       url: `https://www.eeds.com/mobile/hp_signin.aspx?Emulate_App=yes&PIN=${pin}&Sign_in_Code=${signInCode}`,
       title: 'Sign In to an Activity',
@@ -51,58 +55,51 @@ const SignInToEventScreen = ({ navigation }) => {
           alignment="center"
           leftControl={BackAction()}
         />
+
         <Divider />
-        {/* <Text category="h5" style={{ alignSelf: 'flex-start' }}>
-            How would you like to sign in?
-          </Text> */}
 
         <ScrollView
-          keyboardShouldPersistTaps="always"
+          keyboardShouldPersistTaps="handled"
           contentContainerStyle={{
             flex: 1,
             paddingHorizontal: 16,
             paddingVertical: 20,
           }}
         >
-          <TouchableWithoutFeedback
-            accessible={false}
-            onPress={Keyboard.dismiss}
-          >
-            <Layout>
-              <Card header={TextCodeHeader} onPress={Keyboard.dismiss}>
-                <Text>
-                  Enter the activity code displayed at the event to sign in.
-                </Text>
-                <Input
-                  value={signInCode}
-                  size="large"
-                  style={{ marginTop: 12 }}
-                  autoCapitalize="none"
-                  autoCompleteType="off"
-                  autoCorrect={false}
-                  onChangeText={text => setSignInCode(text)}
-                />
-                <Button style={{ marginTop: 12 }} onPress={doSignIn}>
-                  Sign In
-                </Button>
-              </Card>
+          <Layout style={{ flex: 1 }}>
+            <Card header={TextCodeHeader} onPress={Keyboard.dismiss}>
+              <Text>
+                Enter the activity code displayed at the event to sign in.
+              </Text>
+              <Input
+                value={signInCode}
+                size="large"
+                style={{ marginTop: 12 }}
+                autoCapitalize="none"
+                autoCompleteType="off"
+                autoCorrect={false}
+                onChangeText={text => setSignInCode(text)}
+              />
+              <Button style={{ marginTop: 12 }} onPress={doSignIn}>
+                Sign In
+              </Button>
+            </Card>
 
-              <Layout style={{ height: 24 }} />
+            <Layout style={{ height: 24 }} />
 
-              <Card header={QrCodeHeader} onPress={Keyboard.dismiss}>
-                <Text>
-                  Use your device's camera to scan the QR code provided at the
-                  event to sign in.
-                </Text>
-                <Button
-                  style={{ marginTop: 12, alignItems: 'center' }}
-                  onPress={() => navigation.navigate('Camera')}
-                >
-                  Scan QR Code
-                </Button>
-              </Card>
-            </Layout>
-          </TouchableWithoutFeedback>
+            <Card header={QrCodeHeader} onPress={Keyboard.dismiss}>
+              <Text>
+                Use your device's camera to scan the QR code provided at the
+                event to sign in.
+              </Text>
+              <Button
+                style={{ marginTop: 12, alignItems: 'center' }}
+                onPress={() => navigation.navigate('Camera')}
+              >
+                Scan QR Code
+              </Button>
+            </Card>
+          </Layout>
         </ScrollView>
       </SafeAreaView>
     </Layout>
