e545dd4fdd1f7ee950a4958620f23654b59ffee6
Josh White <joshwhite@Joshs-iMac.local>
2020-05-02 10:06:45 -0400

    Implement action sheet for name confirmation

--- screens/auth/LogInScreen.js
+++ screens/auth/LogInScreen.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
+import React, { useRef } from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import axios from 'axios';
-import { Formik } from 'formik';
+import { Formik, useField } from 'formik';
 import {
   Button,
   Divider,
@@ -14,48 +14,104 @@ import {
   TopNavigation,
   TopNavigationAction,
 } from '@ui-kitten/components';
-import { Appearance } from 'react-native-appearance';
+import { useActionSheet } from '@expo/react-native-action-sheet';
 
 import LogInScreenImage from '../../components/LogInScreenImage';
+
+import { useAuth } from '../../context/auth-context';
 
 // Utils
 import getLogInMethodDetails from '../../utils/getLogInMethodDetails';
 
-// Detect which theme the user's device is using
-const deviceThemeSetting = Appearance.getColorScheme();
+const CredentialInput = ({ label, ...props }) => {
+  // eslint-disable-next-line no-unused-vars
+  const [field, meta, helpers] = useField(props);
+  const { value, touched, error } = meta;
+  const { setValue, setTouched } = helpers;
+
+  return (
+    <Input
+      value={value}
+      caption={touched && error ? error : ''}
+      status={touched && error ? 'danger' : 'basic'}
+      size="large"
+      autoFocus={true}
+      autoCorrect={false}
+      autoCapitalize="none"
+      returnKeyType="go"
+      onChangeText={input => setValue(input)}
+      onBlur={setTouched}
+      {...props}
+    />
+  );
+};
 
 const LogInScreen = ({ route, navigation }) => {
   const { logInMethodName, customField } = route.params;
 
+  const { login } = useAuth();
+  const { showActionSheetWithOptions } = useActionSheet();
+
+  const formik = useRef();
+
   const logInMethod = getLogInMethodDetails(logInMethodName, customField);
 
-  // Make sure there's a PIN associated with the crendentials the user entered
-  const fetchPinStatus = async ({ value }, setSubmitting) => {
-    const { data } = await axios.get(logInMethod.url + value);
+  const confirmName = namesArray => {
+    return new Promise(resolve => {
+      const cancelButtonIndex = namesArray.length;
 
-    // Stop if we couldn't find a PIN matching the provided credentials
-    if (data.PIN_Status === false) {
+      showActionSheetWithOptions(
+        {
+          title: 'Please confirm your name',
+          options: [...namesArray, 'Cancel'],
+          cancelButtonIndex: cancelButtonIndex,
+          containerStyle: { maxHeight: '70%' }, // Android only
+        },
+        buttonIndex => {
+          if (buttonIndex === cancelButtonIndex) {
+            resolve();
+          }
+
+          resolve(namesArray[buttonIndex]);
+        }
+      );
+    });
+  };
+
+  const attemptLogin = async ({ value }) => {
+    const accountStatus = await axios
+      .get(logInMethod.url + value)
+      .then(({ data }) => data);
+
+    if (accountStatus.PIN_Status === false) {
       alert(
         'Account Not Found',
         `We couldn't find an account associated with the ${logInMethod.label} you entered.`
       );
-
-      // Let Formik know we're no longer submitting
-      setSubmitting(false);
-
+      formik.current.setSubmitting(false);
       return;
     }
 
-    // Programmatically send user to next screen to confirm their name
-    navigation.navigate('ConfirmName', {
-      pin: data.PIN,
-      namesArray: data.Names_Array,
-      correctName: data.Correct_Name,
-      logInMethodLabel: logInMethod.label,
-    });
+    // Wait for the user to select a name
+    const selectedName = await confirmName(accountStatus.Names_Array);
 
-    // Let Formik know we're no longer submitting
-    setSubmitting(false);
+    // If they cancelled the name selection, just return to the form
+    if (!selectedName) {
+      formik.current.setSubmitting(false);
+      return;
+    }
+
+    // If they selected the wrong name, let them know
+    if (selectedName !== accountStatus.Correct_Name) {
+      formik.current.setSubmitting(false);
+      alert(
+        `The name you selected is not associated with the ${logInMethod.label} you entered.`
+      );
+      return;
+    }
+
+    // If they selected the correct name, log them in
+    login(accountStatus.PIN);
   };
 
   const BackAction = () => (
@@ -75,72 +131,38 @@ const LogInScreen = ({ route, navigation }) => {
         />
         <Divider />
         <ScrollView
-          keyboardShouldPersistTaps="always"
+          keyboardShouldPersistTaps="handled"
           contentContainerStyle={{ paddingBottom: 20 }}
           style={{ flex: 1 }}
         >
-          <Layout
-            style={{ flex: 1, alignItems: 'center', paddingHorizontal: 16 }}
-          >
+          <Layout style={styles.container}>
             <Layout style={{ marginVertical: 20 }}>
               <LogInScreenImage imageUrl={logInMethod.image} />
             </Layout>
             <Formik
+              innerRef={formik}
               initialValues={{ value: '' }}
               validationSchema={logInMethod.validationSchema}
-              onSubmit={(values, { setSubmitting }) =>
-                fetchPinStatus(values, setSubmitting)
-              }
+              onSubmit={attemptLogin}
             >
-              {({
-                values,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                errors,
-                touched,
-                isSubmitting,
-              }) => (
+              {({ handleSubmit, isSubmitting }) => (
                 <>
-                  <Input
+                  <CredentialInput
                     name="value"
                     placeholder={`Enter Your ${logInMethod.label}`}
-                    value={values.value}
-                    size="large"
-                    caption={errors.value && touched.value ? errors.value : ''}
-                    status={errors.value && touched.value ? 'danger' : 'basic'}
                     keyboardType={logInMethod.keyboardType}
-                    keyboardAppearance={
-                      deviceThemeSetting === 'dark' ? 'dark' : 'default'
-                    }
                     maxLength={logInMethodName === 'pin' ? 8 : 50}
-                    returnKeyType="go"
-                    autoFocus={true}
-                    autoCorrect={false}
-                    autoCapitalize="none"
-                    onChangeText={handleChange('value')}
-                    onBlur={handleBlur('value')}
                   />
-                  {logInMethod.instructions ? (
+                  {logInMethod.instructions && (
                     <Text
                       category="c1"
                       appearance="hint"
-                      style={{
-                        alignSelf: 'flex-start',
-                        marginTop: errors.value && touched.value ? 6 : 0,
-                        marginBottom: 12,
-                      }}
+                      style={styles.instructions}
                     >
                       {logInMethod.instructions}
                     </Text>
-                  ) : null}
-                  <Layout
-                    style={{
-                      flexDirection: 'row',
-                      justifyContent: 'center',
-                      marginTop: 6,
-                    }}
-                  >
+                  )}
+                  <Layout style={styles.buttonContainer}>
                     {isSubmitting ? (
                       <Spinner size="large" />
                     ) : (
@@ -160,3 +182,23 @@ const LogInScreen = ({ route, navigation }) => {
 };
 
 export default LogInScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingHorizontal: 16,
+  },
+
+  instructions: {
+    alignSelf: 'flex-start',
+    marginTop: 6,
+    marginBottom: 12,
+  },
+
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 6,
+  },
+});
