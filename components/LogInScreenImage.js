fc0b10a985cd41ece83ad1f95cc94f6c270de0be
Josh White <joshwhite@Joshs-iMac.local>
2020-05-07 09:34:56 -0400

    Use useColorScheme hook

--- components/LogInScreenImage.js
+++ components/LogInScreenImage.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import { Image, useWindowDimensions } from 'react-native';
-import { Appearance } from 'react-native-appearance';
+import { useColorScheme } from 'react-native-appearance';
 
 import Doctors from '../assets/doctors.svg';
 
-// Detect which theme the user's device is using
-const deviceThemeSetting = Appearance.getColorScheme();
-
 const LogInScreenImage = ({ imageUrl }) => {
+  const colorScheme = useColorScheme();
   const windowWidth = useWindowDimensions().width;
 
   // If no custom image associated with the selected log in method, use generic image.
   // TODO: For now, if the user's device is in dark mode, we show the generic image even
   // if we have the sponsor's logo. This is because most logos are not transparent and so
   // look bad on the dark background.
-  if (imageUrl === null || deviceThemeSetting === 'dark') {
+  if (imageUrl === null || colorScheme === 'dark') {
     return <Doctors width={windowWidth * 0.75} style={{ maxHeight: 200 }} />;
   }
 
