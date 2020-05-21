d4a7f2721ed6edc904a83bfc38df5002d26b0cfa
Josh White <joshdwhite@gmail.com>
2020-05-21 16:06:39 -0400

    Update UI and add validation

--- screens/SignInToEventScreen.js
+++ screens/SignInToEventScreen.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Keyboard, ScrollView, StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as WebBrowser from 'expo-web-browser';
+import axios from 'axios';
 import {
   Button,
   Divider,
   Icon,
   Input,
   Layout,
+  Spinner,
   Text,
   TopNavigation,
   TopNavigationAction,
@@ -20,9 +22,50 @@ const SignInToEventScreen = ({ navigation }) => {
   const theme = useTheme();
   const { pin } = useUser();
 
+  const [isBusy, setIsBusy] = useState(false);
   const [signInCode, setSignInCode] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  // Clear error message when text is changed to prevent stale messages
+  useEffect(() => {
+    setErrorMessage('');
+  }, [signInCode]);
+
+  const validateSignInCode = async () => {
+    if (signInCode.length !== 6) {
+      return {
+        isValid: false,
+        errorMessage: 'The sign-in code should be exactly six characters.',
+      };
+    }
+
+    const codeStatus = await axios
+      .get(
+        `https://www.eeds.com/ajax_functions.aspx?Function_ID=133&Sign_In_Code=${signInCode}`
+      )
+      .then(({ data }) => data);
+
+    return {
+      isValid: codeStatus.Is_Valid,
+      errorMessage: 'The sign-in code you entered is not valid.',
+      // FIXME: The server returns a more useful error message, but it sometimes contains
+      // HTML. If that were changed, we could use the server message.
+      // errorMessage: codeStatus.Error_Message,
+    };
+  };
 
   const signInToActivity = async () => {
+    setIsBusy(true);
+
+    const codeStatus = await validateSignInCode();
+
+    setIsBusy(false);
+
+    if (!codeStatus.isValid) {
+      setErrorMessage(codeStatus.errorMessage);
+      return;
+    }
+
     await WebBrowser.openBrowserAsync(
       `https://www.eeds.com/mobile/hp_signin.aspx?Emulate_App=yes&PIN=${pin}&Sign_in_Code=${signInCode}`
     );
@@ -70,8 +113,8 @@ const SignInToEventScreen = ({ navigation }) => {
               </Text>
               <Divider />
               <View style={styles.cardBody}>
-                <Text>
-                  Enter the activity code displayed at the event to sign in.
+                <Text style={{ fontSize: 16 }}>
+                  Enter the sign-in code displayed at the event to sign in.
                 </Text>
                 <Input
                   value={signInCode}
@@ -80,11 +123,28 @@ const SignInToEventScreen = ({ navigation }) => {
                   autoCapitalize="none"
                   autoCompleteType="off"
                   autoCorrect={false}
+                  maxLength={6}
+                  caption={errorMessage}
+                  status={errorMessage ? 'danger' : 'basic'}
                   onChangeText={text => setSignInCode(text)}
                 />
-                <Button style={{ marginTop: 12 }} onPress={signInToActivity}>
-                  Sign In
-                </Button>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: 43, // Prevent jank when switching btwn button and spinner
+                    marginTop: 12,
+                  }}
+                >
+                  {isBusy ? (
+                    <Spinner size="large" />
+                  ) : (
+                    <Button style={{ flex: 1 }} onPress={signInToActivity}>
+                      Sign In
+                    </Button>
+                  )}
+                </View>
               </View>
             </View>
 
@@ -100,7 +160,7 @@ const SignInToEventScreen = ({ navigation }) => {
               </Text>
               <Divider />
               <View style={styles.cardBody}>
-                <Text>
+                <Text style={{ fontSize: 16 }}>
                   Use your device's camera to scan the QR code provided at the
                   event to sign in.
                 </Text>
