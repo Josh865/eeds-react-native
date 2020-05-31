10b2859472f571cee96720f81f3711872b207fb9
Josh White <joshdwhite@gmail.com>
2020-05-31 09:42:45 -0400

    Remove old auth context

--- AuthContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const AuthContext = React.createContext();
-
-export const AuthContextProvider = props => (
-  <AuthContext.Provider value={props.value}>
-    {props.children}
-  </AuthContext.Provider>
-);
