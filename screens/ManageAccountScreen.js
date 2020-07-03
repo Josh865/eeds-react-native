85effaac38c64ce6862e116b2aea1840a90398ee
Josh White <joshwhite@Joshs-MacBook-Pro.local>
2020-07-03 15:47:07 -0400

    Build manage account screen

--- screens/ManageAccountScreen.js
+++ screens/ManageAccountScreen.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import {
   Button,
@@ -11,9 +12,11 @@ import {
 } from '@ui-kitten/components';
 
 import { useAuth } from '../context/auth-context';
+import { useUser } from '../context/user-context';
 
 const ManageAccountScreen = ({ navigation }) => {
   const { logout } = useAuth();
+  const { pin, fullName } = useUser();
 
   const BackAction = () => (
     <TopNavigationAction
@@ -33,12 +36,36 @@ const ManageAccountScreen = ({ navigation }) => {
 
         <Divider />
 
-        <Text>Hi</Text>
+        <Layout level="3" style={{ flex: 1, paddingHorizontal: 16 }}>
+          <Layout level="1" style={{ marginTop: 24, borderRadius: 5 }}>
+            <View style={styles.infoLine}>
+              <Text>Name</Text>
+              <View>
+                <Text>{fullName}</Text>
+              </View>
+            </View>
+            <Divider />
+            <View style={styles.infoLine}>
+              <Text>PIN</Text>
+              <Text>{pin}</Text>
+            </View>
+          </Layout>
 
-        <Button onPress={logout}>Log Out</Button>
+          <Button style={{ marginTop: 16 }} onPress={logout}>
+            Log Out
+          </Button>
+        </Layout>
       </SafeAreaView>
     </Layout>
   );
 };
+
+const styles = StyleSheet.create({
+  infoLine: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 16,
+  },
+});
 
 export default ManageAccountScreen;
